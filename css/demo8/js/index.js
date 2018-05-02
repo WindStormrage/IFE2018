2207a31bb46f9bc5c896c48540cc55330c886cc5
xhy <695729033@qq.com>
2018-05-02 17:28:02 +0800

    css8  鼠标触发版本

--- css/demo8/js/index.js
+++ css/demo8/js/index.js
@@ -1,14 +1,24 @@
 var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: './json/data.json'
+	container: document.getElementById('bm'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: './json/data.json'
 })
 var animation2 = bodymovin.loadAnimation({
-  container: document.getElementById('man'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: './json/data32.json'
+	container: document.getElementById('man'),
+	renderer: 'svg',
+	loop: false,//循环
+	autoplay: false,//是否自动播放
+	path: './json/data32.json'
+})
+
+
+document.getElementById("man").addEventListener("mouseover",function (e) {
+	console.log(e);
+	animation2.play()
+})
+document.getElementById("man").addEventListener("mouseout",function (e) {
+	console.log(e);
+	animation2.stop()
 })
